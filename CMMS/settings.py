0701920a308b6d530fe87acb62cd2f8dd601ac73
AboAlMike <shawwara1@gmail.com>
2025-07-21 03:56:42 +0300

    sec

--- CMMS/settings.py
+++ CMMS/settings.py
@@ -145,7 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['AssetMate.pythonanywhere.com']
 
 
 
